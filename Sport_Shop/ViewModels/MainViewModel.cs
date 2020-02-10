12d7827897c6308478c0afc047ccb91174b06776
Никита Целков <nikitacelkov@gmail.com>
2020-02-10 18:13:26 +0700

    Добавьте файлы проекта.

---
+++ Sport_Shop/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Sport_Shop.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        public int Clicks { get; set; }
+        
+
+       
+        public ICommand ClickAdd
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    Clicks++;
+                });
+            }
+        }
+
+    }
+}
